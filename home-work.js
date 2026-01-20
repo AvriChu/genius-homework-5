1abb45c87a9ea60fe666b6f36968aceeba446823
(Avi) Anastasia <nastona367@gmail.com>
2026-01-20 22:51:16 +0200

    Made homework 1

---
+++ home-work.js
@@ -0,0 +1,69 @@
+// Завдання 1
+// let a = 0;
+
+// a > 0 ? console.log(true) : console.log(false);
+
+// Завдання 2
+// let a = 'test';
+// a === 'test' ? console.log(true) : console.log(false);
+
+// Завдання 3
+// let a = 0;
+
+// a > 10 ? console.log(a - 5) : console.log(a + 5);
+
+// Завдання 4
+// const nameMonth = Number(prompt('Введіть номер місяця'));
+
+// switch (nameMonth) {
+//   case 1:
+//     console.log('Січень');
+//     break;
+//   case 2:
+//     console.log('Лютий');
+//     break;
+//   case 3:
+//     console.log('Березень');
+//     break;
+//   case 4:
+//     console.log('Квітень');
+//     break;
+//   case 5:
+//     console.log('Травень');
+//     break;
+//   case 6:
+//     console.log('Червень');
+//     break;
+//   case 7:
+//     console.log('Липень');
+//     break;
+//   case 8:
+//     console.log('Серпень');
+//     break;
+//   case 9:
+//     console.log('Вересень');
+//     break;
+//   case 10:
+//     console.log('Жовтень');
+//     break;
+//   case 11:
+//     console.log('Листопад');
+//     break;
+//   case 12:
+//     console.log('Грудень');
+//     break;
+//   default:
+//     console.log('Данні введені не вірно');
+//     break;
+// }
+
+// Завдання 5
+// const num = parseInt(prompt('Введіть тризначне число'));
+// if (num >= 100 && num <= 999) {
+//   let a = Math.floor(num / 100);
+//   let b = Math.floor((num % 100) / 10);
+//   let c = num % 10;
+//   console.log('Сума = ', a + b + c);
+// } else {
+//   console.log('Данні не вірні!');
+// }
