018467ca540e09421af8071f5cd3d1f4c989a8f0
(Avi) Anastasia <nastona367@gmail.com>
2026-01-24 00:28:17 +0200

    Made Module 24

---
+++ home-work_3.js
@@ -0,0 +1,33 @@
+// Завдання 1
+
+// function checkAge(age) {
+//   return age > 18 ? true : confirm('Батьки дозволили?');
+// }
+
+// checkAge(20);
+
+// Завдання 2
+
+// function min(a, b) {
+//   return a > b
+//     ? console.log(b + ' найменший.')
+//     : console.log(a + ' найменший.');
+// }
+
+// min(5, 5);
+
+// Завдання 3
+
+// const ask = (question, yes, no) => {
+//   return confirm(question) ? yes() : no();
+// };
+
+// ask(
+//   'Ви згодні?',
+//   function () {
+//     alert('Ви погодились.');
+//   },
+//   function () {
+//     alert('Ви скасували виконання.');
+//   },
+// );
